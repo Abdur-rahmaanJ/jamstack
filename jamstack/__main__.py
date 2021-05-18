3ab41107c619b4b1a9d1f8f0b78259ff2baa4cf7
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2021-05-18 19:56:06 -0400

    Template selection improved
    Now you can select the templates by passing as an argument only the name of this one

--- jamstack/__main__.py
+++ jamstack/__main__.py
@@ -1,11 +1,12 @@
 import os
 from pathlib import Path
-from jamstack.api.file import trycopytree
 
 import click
+from jamstack.api.file import trycopytree
 
 package_folder = Path(__file__).parent.absolute()
 sites_path = os.path.join(package_folder, 'sites')
+
 
 @click.group(help="<Jamstack cli/>")
 def cli():
@@ -25,7 +26,7 @@ def plain(project_name, existing):
         os.path.join(sites_path, 'plain'),
         os.path.join(path, project_name),
         dirs_exist_ok=dirs_exist_ok
-        )
+    )
 
 
 @click.command(help="<Repo by templates />")
@@ -34,25 +35,29 @@ def plain(project_name, existing):
 @click.option('--existing/--not-existing', default=False)
 def t(template, project_name, existing):
     path = '.'
+    templates = {'massively': 'html5up/massively',
+                 'phantom': 'html5up/phantom'}
 
-    namespace = template.split('/')[0]
-    project = template.split('/')[1]
+    #namespace = template.split('/')[0]
+    #project = template.split('/')[1]
 
     dirs_exist_ok = False
     if existing is True:
         dirs_exist_ok = True
     trycopytree(
-        os.path.join(sites_path, namespace, project),
+        os.path.join(sites_path, templates[template]),
         os.path.join(path, project_name),
         dirs_exist_ok=dirs_exist_ok
-        )
+    )
 
 
 cli.add_command(plain)
 cli.add_command(t)
 
+
 def main():
     cli()
+
 
 if __name__ == "__main__":
     cli()
